528a8e6be191a30c0cadea43d5fc764e81f0370e
kholilurrohman <odienkzkholil@gmail.com>
2025-04-28 12:39:52 +0700

    membuat route ke hadist.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,22 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+// app/index.jsx
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
 import { useRouter } from 'expo-router';
 
-
-const index = () => {
+const Index = () => {
   const router = useRouter();
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 40 }}>SELAMAT DATANG</Text>
-      <TouchableOpacity style={{ padding: 20, backgroundColor: 'yellow', borderRadius: 10, }} onPress={() => router.push({})pathname: '/Hadist' }>
-        <Text>GET STARTED</Text>
+      <TouchableOpacity 
+        style={{ padding: 20, backgroundColor: 'yellow', borderRadius: 10, marginTop: 20 }} 
+        onPress={() => router.push('/hadist')}
+      >
+        <Text style={{ fontWeight: 'bold' }}>GET STARTED</Text>
       </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-export default index
+export default Index;
